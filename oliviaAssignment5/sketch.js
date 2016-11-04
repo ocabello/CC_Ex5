bb51728bf52bdffb951dea66b0eaa7a069086149
ocabello <ocabello2@gmail.com>
2016-11-04 14:59:19 -0400

    made array of images and created textDuplicate()

--- oliviaAssignment5/sketch.js
+++ oliviaAssignment5/sketch.js
@@ -1,19 +1,20 @@
 var font;
 var paragraph = "Centuries and centuries of idealism have not failed to influence reality. In the very oldest regions of Tlon, it is not an uncommon occurrence for lost objects to be duplicated. Two people are looking for a pencil. The first one finds it and says nothing. The second finds a second pencil, no less real, but more in keeping with his expectation. These secondary objects are called hronir and, even though awkward in form, are a little larger than the originals. Until recently, the hronir were the accidental children of absent mindedness and forgetfulness.";
 var words = paragraph.split(' ');
-var pencil, glasses, object, chess;
 
+var picnum = 4;
+var index = 0;
+var pictures = new Array(picnum);
 
 var xpos = 0;
 var ypos = 0;
 
+
 function preload() {
   font = loadFont("data/font1.otf");
-  // paragraph = loadStrings("data/borges.txt");
-  pencil = loadImage("data/pencil.jpg");
-  glasses = loadImage("data/glasses.jpg");
-  object = loadImage("data/object.jpg");
-  chess = loadImage("data/chess.jpg");
+  for (var i = 0; i < pictures.length; i++) {
+    pictures[i] = loadImage("data/object" + i + ".jpg");
+  }
 
 }
 
@@ -23,43 +24,45 @@ function setup() {
   stroke(3);
   fill(0, 50, 80);
   textFont(font, 36);
+
+
   //noLoop();
 }
 
 function draw() {
   //background(255);
 
-
-
   //imageColorChange()
-
-  //image(object, 0, 0);
-  //image(chess, 0, 256);
-
   var margin = 20;
   var txtX = margin;
   var txtY = 50;
-  for (var i = 0; i < words.length; i++) {
+  
+if (keyIsPressed){
+  if (key == '8'){
+      noStroke();
+      fill(255,102,178, random(0, 5) );
+      textDuplicate(20, 475);
+  }
 
-    if (words[i] == 'hronir') {
-      //println(words)
-      //println("hello");
-      fill(255, 255, 0);
-    } 
-     else if (words[i] == 'reality.') {
-      fill(0, 0, 255);
-      
+}
+
+
+  for (var i = 0; i < words.length; i++) {
+    
+    
+      if (words[i] == 'reality.') {
+      //fill(0, 0, 255);
+
       if (mouseIsPressed) {
-          fill(random(255), random(255), random(255));
+        fill(random(255), random(255), random(255));
       }
-    }
-    else if (words[i] == 'idealism') {
-      fill(0, 255, 0);
-      
+    } else if (words[i] == 'idealism') {
+      //fill(0, 255, 0);
+
       if (keyIsPressed) {
-        if (key = 'i') {
+        if (key == 'i') {
           fill(random(255), random(255), random(255));
-          imageLoop(object);
+          imageLoop(random(pictures));
         }
       }
     } else {
@@ -67,26 +70,44 @@ function draw() {
     }
 
 
-
     var txtWidth = textWidth(words[i] + ' ');
     if ((txtX + txtWidth) > (width - margin)) {
       txtX = margin;
-      txtY += 36;
+      txtY += 45;
     }
 
     text(words[i], txtX, txtY);
     txtX += txtWidth;
 
-
   }
 
 
-
 }
 
-function textJitter() {
 
+
+function textDuplicate(locX, locY) {
+  var px = locX;
+  var py = locY;
+  var characters = new Array(words.length);
+  var speed = 1;
+  
+  for (var i = 0; i < words.length; i++) {
+    for (var j = 0; j < words[i].length; j++) {
+      //  println(words[i][j]);
+
+      if (words[i] == 'hronir') {
+        // println(words[i][j]);
+        var charw = textWidth(words[i][j]);
+      //  println(w);
+        px += charw;
+        text(words[i][j], px, py);
+     }
+
+    }
+  }
 }
+
 
 function imageLoop(pic) {
   var img = pic;
